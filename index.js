fbd23658d0895e784ffb725c0ec8f3aca2e2ee94
harshitdelhivery <harshit.malhotra@delhivery.com>
2017-06-15 15:03:51 +0530

    Time function [aws lambda based]
    Following
    http://docs.aws.amazon.com/lambda/latest/dg/automating-deployment.html

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by hmalhotra on 15/06/17.
+ */
+var time = require('time');
+exports.handler = (event, context, callback) => {
+    var currentTime = new time.Date();
+    currentTime.setTimezone("America/Los_Angeles");
+    callback(null, {
+        statusCode: '200',
+        body: 'The time in Los Angeles is: ' + currentTime.toString(),
+    });
+};
